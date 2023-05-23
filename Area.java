df38c5f54ef8f4df9270276d6f6ccf6d6b0fa37e
Bharanimr <rajabharani2204@gmail.com>
2023-05-23 20:18:09 +0530

    Method overloading

---
+++ Area.java
@@ -0,0 +1,18 @@
+class Area
+{
+
+public static void main(String[] args)
+
+{
+    Area square = new Area();
+    Area rectangle = new Area();
+    square.calculate(40);
+
+
+}
+void calculate(int a)
+{
+    System.out.println(a*a);
+}
+
+}
