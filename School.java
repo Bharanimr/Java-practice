35e129c9a339df4b25b154638e1816d62e19dd3d
Bharanimr <rajabharani2204@gmail.com>
2023-05-24 21:05:29 +0530

    Field initialization

---
+++ School.java
@@ -0,0 +1,37 @@
+class School
+{
+int salary;
+//Fields [Global variable] will be initialized with their default values(0)
+
+boolean b;
+char c;
+byte by;
+short s;
+long l;
+float f;
+double d;
+String St;
+public static void main(String[] args)
+{
+School teacher = new School();
+teacher.teach();
+System.out.println("int " +teacher.salary);
+System.out.println("boolean " +teacher.b);
+System.out.println("char " +teacher.c);
+System.out.println("byte "+teacher.by);
+System.out.println("short "+teacher.s);
+System.out.println("long "+teacher.l);
+System.out.println("float "+teacher.f);
+System.out.println("double "+teacher.d);
+System.out.println("String "+teacher.St);
+
+}
+void teach()
+{
+int ab;
+System.out.println(ab);
+
+
+}
+
+}
