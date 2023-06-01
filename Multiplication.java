14e4d65afc7df7b6a004d872d37445789046115e
Bharanimr <rajabharani2204@gmail.com>
2023-06-01 17:47:13 +0530

    Using Multiplication of first 5 Numbers program

---
+++ Multiplication.java
@@ -0,0 +1,20 @@
+package com.bharani;
+
+public class Multiplication 
+{
+
+	public static void main(String[] args) 
+	{
+		int num=1;
+		int total=1;
+		
+		while(num<=5)
+		{
+			total=total*num;
+			num=num+1;
+			
+		}
+		System.out.println(total);
+	}
+
+}
