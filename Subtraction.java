3025b0213342bae95e751456ff44764a8a0adc9f
Bharanimr <rajabharani2204@gmail.com>
2023-06-01 17:46:31 +0530

    Using Subtraction of first 5 Numbers program

---
+++ Subtraction.java
@@ -0,0 +1,20 @@
+package com.bharani;
+
+public class Subtraction 
+{
+
+	public static void main(String[] args) 
+	{
+		int num=1;
+		int total=0;
+		
+		while(num<=5)
+		{
+			total=total-num;
+			num=num+1;
+			
+		}
+		System.out.println(total);
+	}
+
+}
