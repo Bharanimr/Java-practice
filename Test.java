2f0010ba21bba1121af12d7d4d61632b5d24f2e9
Bharanimr <rajabharani2204@gmail.com>
2023-06-01 17:49:02 +0530

    Using If Else condition

---
+++ Test.java
@@ -0,0 +1,20 @@
+class Test
+{
+public static void main(String[] args)
+{
+Test rect = new Test();
+int l = 10;
+int b= 5;
+
+
+if (l==b)
+{
+System.out.println("Square");
+}
+else
+{
+System.out.println("rect");
+}
+
+}
+}
