77337fbe4ab767a49c117153db7c1062628217a8
Bharanimr <rajabharani2204@gmail.com>
2023-06-01 17:40:37 +0530

    Using Addition of first 5 Numbers program

---
+++ Addition.java
@@ -0,0 +1,21 @@
+package com.bharani;
+
+public class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+		int num=1;
+		int total=0;
+		
+		while(num<=5)
+		{
+			total=total+num;
+			num=num+1;
+			
+		}
+		System.out.println(total);
+
+	}
+
+}
