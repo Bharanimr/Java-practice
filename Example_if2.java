c3e3cf87bee0069f7351562466314ae21df188c5
Bharanimr <rajabharani2204@gmail.com>
2023-06-11 21:20:38 +0530

    Simple if program

---
+++ Example_if2.java
@@ -0,0 +1,19 @@
+package com.bharani;
+
+public class Example_if2 
+{
+
+	public static void main(String[] args) 
+	{
+		int year=2020;    
+	    if(year % 4 ==0)
+	    {  
+	        System.out.println("LEAP YEAR");  
+	    }  
+	    else
+	    {  
+	        System.out.println("COMMON YEAR");  
+	    }  
+	}
+
+}
