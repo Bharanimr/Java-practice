67a8e0febc187fd23709af68b6d1c4f77c41d6e0
Bharanimr <rajabharani2204@gmail.com>
2023-06-11 21:20:03 +0530

    Simple if program

---
+++ Example_if.java
@@ -0,0 +1,22 @@
+package com.bharani;
+
+public class Example_if 
+{
+
+	public static void main(String[] args) 
+	{
+		//defining an 'age' variable  
+		int age=10;  
+		//checking the age 
+		
+		if(age>=18)
+		{  
+			System.out.print("Eligible for voting");  
+		}
+		else
+		{
+			System.out.println("Not eligible for voting");
+		}
+	}
+
+}
