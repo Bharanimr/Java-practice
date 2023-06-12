4ec7aa8fe3d6ddc52414e70a1862b8f5277d37b8
Bharanimr <rajabharani2204@gmail.com>
2023-06-12 20:46:43 +0530

    Basic increment program

---
+++ Increment.java
@@ -0,0 +1,16 @@
+package com.bharani;
+
+public class Increment 
+{
+
+	public static void main(String[] args) 
+	{
+		int no =10;
+		System.out.println(no++); //post increment
+		System.out.println(no);
+		
+		System.out.println(++no); //pre increment
+		System.out.println(no);
+	}
+
+}
