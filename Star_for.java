392b95e4afc27e1dc439be518026155f4f792a20
Bharanimr <rajabharani2204@gmail.com>
2023-06-12 20:47:28 +0530

    Using for loop star program

---
+++ Star_for.java
@@ -0,0 +1,24 @@
+package com.bharani;
+
+public class Star_for 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int star=1; star<=7; star++)
+		  {
+		    System.out.print("* ");
+		  }
+		  System.out.println();
+		  for(int star=1; star<=5; star++)
+		  {
+		    System.out.println("* ");
+		  }
+		  for(int star=1; star<=7; star++)
+		  {
+		    System.out.print("* ");
+		  }
+		  System.out.println();
+	}
+
+}
