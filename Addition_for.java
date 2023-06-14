620a1651457fd0ca49ce624eec1da013fca0d6fb
Bharanimr <rajabharani2204@gmail.com>
2023-06-14 22:49:07 +0530

    Addition using for loop

---
+++ Addition_for.java
@@ -0,0 +1,17 @@
+package com.bharani;
+
+public class Addition_for 
+{
+
+	public static void main(String[] args) 
+	{
+		int total=0;
+		for(int num=1;num<=5;num++)
+		{
+			System.out.println(num);
+			total=total+num;
+		}
+		System.out.println(total);
+	}
+
+}
