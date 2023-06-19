51d1237dbfaa775a74094e73dc6cd00f398835b4
Bharanimr <rajabharani2204@gmail.com>
2023-06-19 19:12:19 +0530

    Using for loop

---
+++ Neon_for.java
@@ -0,0 +1,28 @@
+package com.bharani;
+
+public class Neon_for 
+{
+
+	public static void main(String[] args) 
+	{
+		int no=9;
+		int res=0;
+		int rem=no*no;
+		System.out.println(rem);
+		for(;rem>0;rem=rem/10)  //while(rem>0)
+		{
+			int no2=rem%10;
+		    res=no2+res; 
+		}
+		System.out.println(res);
+		if(res==no)
+		{
+			System.out.println(no+ " is a Neon number");
+		}
+		else
+		{
+			System.out.println(no+ " is not a Neon number");
+		}
+	}
+
+}
