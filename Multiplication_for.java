2b2842ac08664b3e3053755b77a85262d1d2ebcd
Bharanimr <rajabharani2204@gmail.com>
2023-06-19 19:11:56 +0530

    Using for loop

---
+++ Multiplication_for.java
@@ -0,0 +1,18 @@
+package com.bharani;
+
+public class Multiplication_for 
+{
+
+	public static void main(String[] args) 
+	{
+		int total=1;
+		
+		for(int num=1;num<=5;num++)
+		{
+			total=total*num;
+			
+		}
+		System.out.println(total);
+	}
+
+}
