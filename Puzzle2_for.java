2b33d786da338f4cbdef40178d56d4eb1375d9a2
Bharanimr <rajabharani2204@gmail.com>
2023-06-21 21:51:00 +0530

    Using for loop

---
+++ Puzzle2_for.java
@@ -0,0 +1,26 @@
+package com.bharani;
+
+public class Puzzle2_for 
+{
+
+	public static void main(String[] args) 
+	{
+		Puzzle2_for plan = new Puzzle2_for();
+		plan.father_plan();
+	}
+	void father_plan()
+	{
+	
+		int savings = 0;
+		
+		for(int day=1;day<=10;day++)
+			{
+				savings = savings+day;
+				
+				System.out.println("Daily:" +savings);
+			}
+		System.out.println("Total Savings is:" +savings);
+	}
+
+
+}
