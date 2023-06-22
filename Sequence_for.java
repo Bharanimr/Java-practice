bb6240a9f4534677ead5210a2b19e73ca5fab08f
Bharanimr <rajabharani2204@gmail.com>
2023-06-22 20:34:59 +0530

    Using for loop

---
+++ Sequence_for.java
@@ -0,0 +1,20 @@
+package com.bharani;
+
+public class Sequence_for 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		for(int no=1;no<=10;no+=2)        
+		{
+		    System.out.print(no+" "); 
+		    if(no==9)
+		        {
+		        	no = 0; 
+		        }
+		}		
+
+	}
+
+}
