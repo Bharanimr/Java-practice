21635ddf3e650dee2a2e7a8aa00d42f5e1b1736e
Bharanimr <rajabharani2204@gmail.com>
2023-06-24 17:56:11 +0530

    Using for loop

---
+++ Subtraction_for.java
@@ -0,0 +1,20 @@
+package com.bharani;
+
+public class Subtraction_for 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int total=0;
+		
+		for(int num=1;num<=5;num++)
+		{
+			total=total-num;
+			
+			
+		}
+		System.out.println(total);
+	}
+
+}
