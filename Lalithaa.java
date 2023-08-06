616f91f847caac79cb490016b7f4d589d9776b00
Bharanimr <rajabharani2204@gmail.com>
2023-08-06 18:33:42 +0530

    Using constructor this()

---
+++ Lalithaa.java
@@ -0,0 +1,23 @@
+package com.bharani;
+
+public class Lalithaa 
+{
+	 int amt; 
+	  
+	  public Lalithaa(int amt) 
+	  {
+		  this();
+		  this.amt = amt; 
+		  System.out.println("Goddess Gift");
+	  }
+	public Lalithaa() 
+	{
+		System.out.println("Cool Drinks");
+	}
+	public static void main(String[] args) 
+	{
+		 Lalithaa harish_ring = new Lalithaa(10000);
+		 Lalithaa bharani = new Lalithaa();
+	}
+
+}
